9c9fe08d12605b044cd59d64c874348e7c042418
Geovannalf <geovannalima0205@gmail.com>
2022-09-26 23:07:23 -0300

    Lógica de cálculo e validação de dados terminado

--- app/src/main/java/br/com/gastosviagens/MainActivity.kt
+++ app/src/main/java/br/com/gastosviagens/MainActivity.kt
@@ -1,11 +1,12 @@
-
 package br.com.gastosviagens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import br.com.gastosviagens.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,8 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-    private fun calculate(){
 
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != "" &&
+                binding.editPrice.text.toString() != "" &&
+                binding.editAutonomy.text.toString() != "" &&
+                binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+
+            binding.textTotalValue.text = "%.2f".format(totalValue)
+
+            //Toast.makeText(this, totalValueStr.toString(), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_LONG).show()
+        }
     }
 }
